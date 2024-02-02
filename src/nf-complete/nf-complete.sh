46e6eb7b9dfd5c86dd8e64827e7e336b8fbbda03
5uperb0y <https://github.com/5uperb0y>
2024-02-02 22:42:36 +0800

    feat(nf-complete): exclude cmd option in fzf menu
    this ensure that each option is only selected once.
    (latter option value overwrites previous one in Nextflow CLI)

--- src/nf-complete/nf-complete.sh
+++ src/nf-complete/nf-complete.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
+extract_available_options () {
+	awk '
+		/"""/ { inDoc = !inDoc; next}
+		inDoc && /^Options/ { inOpt = 1; next }
+		inDoc && inOpt && /\s*-/ { print }
+		inDoc && !inOpt && /^$/ { inOpt = 0 }
+		' "$1"
+}
+append_to_cmd () {
+	local text="$1"
+	READLINE_LINE="${READLINE_LINE}${text} "
+	READLINE_POINT=$(( READLINE_POINT + ${#text} + 1 ))
+}
 _nf_complete(){
-	local cmd_line=${READLINE_LINE}
-	local -a words=(${cmd_line})
-	if [[ "${words[0]}" == "nextflow" ]] && [[ "${words[1]}" == "run" ]] && [[ "${words[2]}" == *.nf ]]; then
-		nf_script="${words[2]}"
-		selected=$(awk '
-			/"""/ { inDoc = !inDoc; next}
-			inDoc && /^Options/ { inOpt = 1; next }
-			inDoc && inOpt && /\s*-/ { print }
-			inDoc && !inOpt && /^$/ { inOpt = 0 }
-			' ${nf_script} | sed 's/^\s//' | fzf --height 20% --reverse --header="${cmd_line}" | awk -F " " '{print $1}')
-		READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}${selected} ${READLINE_LINE:$READLINE_POINT}"
-		READLINE_POINT=$(( READLINE_POINT + ${#selected} + 1 ))
+	local cmd="${READLINE_LINE}"
+	local -a words=(${cmd})
+	if [[ "${words[0]}" == "nextflow" && "${words[1]}" == "run" && "${words[2]}" == *.nf ]]; then
+		local nf_script="${words[2]}"
+		local cmd_options=$(echo "${cmd}" | grep -o -E -- "--?\w*")
+		local exclusions=$(echo ${cmd_options} | sed 's/ /|/g') 
+		local options=$(extract_available_options "${nf_script}" | grep -v -E -- "${exclusions:-\$^}")
+		local selected_option=$(
+			echo "${options}" |\
+			sed 's/^\s//' |\
+			fzf --height 20% --reverse --header="${cmd}" |\
+			awk -F " " '{print $1}'
+			)
+		append_to_cmd "${selected_option}"
 	fi
 }
 
